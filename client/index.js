74383c1fb01f0e7e1691a81f1e11b91c58c11f9f
JerryLin1 <jerreelynn@gmail.com>
2021-08-09 10:57:21 -0400

    O

--- client/index.js
+++ client/index.js
@@ -19,22 +19,19 @@ var game = new Phaser.Game({
         update: update
     }
 });
-function preload ()
-{
-}
-var t
-var r1
-function create ()
-{
-    this.cameras.main.backgroundColor.setTo(51, 51, 51); 
-    r1 = this.add.rectangle(0, 0, 148, 148, 0x6666ff);
-
-    t = 0;
+function preload() {
+    this.objects = {}
+    
 }
 
-function update ()
-{
-    r1.y = 300 + Math.cos(t) * 100;
-    r1.x = 300 + Math.cos(t) * 100;
-    t+=0.02;
+function create() {
+    this.cameras.main.backgroundColor.setTo(51, 51, 51);
+    this.objects.rectangle = this.add.rectangle(0, 0, 148, 148, 0x6666ff);
+    this.objects.t = 0;
+}
+
+function update() {
+    this.objects.rectangle.y = 300 + Math.cos(this.objects.t) * 100;
+    this.objects.rectangle.x = 300 + Math.sin(this.objects.t) * 100;
+    this.objects.t += 0.02;
 }
