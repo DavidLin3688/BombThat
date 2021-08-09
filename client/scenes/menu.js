68a79608c1507fe7c5d7c949d303c19f89a104ac
JerryLin1 <jerreelynn@gmail.com>
2021-08-09 16:27:33 -0400

    idk

--- client/scenes/menu.js
+++ client/scenes/menu.js
@@ -10,12 +10,16 @@ export default class Menu extends Phaser.Scene {
     create() {
         this.cameras.main.backgroundColor.setTo(51, 51, 51);
         this.objects.rectangle = this.add.rectangle(0, 0, 148, 148, 0x6666ff);
+        this.objects.titleText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, "💣 BOMB THAT! 💣", {
+            fontSize: 50,
+            padding: { x: 100, y: 100 }
+        }).setOrigin(0.5).setShadow(0, 0, "black", 30, true, true);
         this.objects.t = 0;
     }
 
     update() {
         this.objects.rectangle.y = 300 + Math.cos(this.objects.t) * 100;
-        this.objects.rectangle.x = 300 + Math.sin(this.objects.t) * 100;
+        this.objects.rectangle.x = 300 + Math.tan(this.objects.t) * 100;
         this.objects.t += 0.02;
     }
 }
