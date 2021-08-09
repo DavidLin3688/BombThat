df5aa146b61762d3cee9c2fae35b8bb841903055
JerryLin1 <jerreelynn@gmail.com>
2021-08-09 10:51:12 -0400

    rectangle.

--- client/index.js
+++ client/index.js
@@ -11,5 +11,30 @@ var game = new Phaser.Game({
         arcade: {
             gravity: { y: 200 }
         }
+    },
+
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
 });
+function preload ()
+{
+}
+var t
+var r1
+function create ()
+{
+    this.cameras.main.backgroundColor.setTo(51, 51, 51); 
+    r1 = this.add.rectangle(0, 0, 148, 148, 0x6666ff);
+
+    t = 0;
+}
+
+function update ()
+{
+    r1.y = 300 + Math.cos(t) * 100;
+    r1.x = 300 + Math.cos(t) * 100;
+    t+=0.02;
+}
